3291e9f6869d7ec9703de3d08702df57f984dadf
Jet <390715001@qq.com>
2025-04-24 17:31:17 +0800

    update

---
+++ app/dashboard/settings/error.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const ErrorComponent = () => {
+  return (
+    <div>
+      <h1>Error Occurred!</h1>
+      <p>Something went wrong.</p>
+    </div>
+  );
+};
+
+export default ErrorComponent;
