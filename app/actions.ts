09e575b132e9b34308042ae0b504994a9e084f40
Jet <390715001@qq.com>
2025-04-30 15:37:02 +0800

    change the ReadMe.md

--- app/actions.ts
+++ app/actions.ts
@@ -1,29 +1,53 @@
 "use server";
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { useActionStat } from 'react';
+
 
 export async function createPost(formData: FormData) {
     const title = formData.get("title");
     const content = formData.get("content");
 
     // Update data
-    return new Promise((resolve) =>{
+    // return new Promise((resolve) =>{
+    //     // Revalidate cache
+    //     revalidatePath('/posts')
+    //     // redirect the user to a different page after performing an update
+    //     redirect('/posts');
+
+    //     resolve({
+    //         title,
+    //         content,
+    //         author: 'John Doe',
+    //         tags: ['nextjs', 'react', 'nodejs'],
+    //     })
+    // })
 
 
-        // Revalidate cache
-        revalidatePath('/posts')
-        // redirect the user to a different page after performing an update
-        redirect('/posts');
 
-        resolve({
+    const res = await fetch('https://api.vercel.app/posts', {
+        method: 'POST',
+        body: {
             title,
             content,
-            author: 'John Doe',
-            tags: ['nextjs', 'react', 'nodejs'],
-        })
+        },
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
     })
 
-
+    const json = await res.json();
+    if(!res.ok) {
+        // throw new Error('Failed to create post')
+        return {
+            message: "Failed to create post",
+            error: json.message,
+        }
+    }else{
+        // revalidatePath('/posts')
+        // redirect('/posts');
+        return json;
+    }
 }
 
 
